37a09f7811e73d177677bc866668ad9323459ed2
Chell <116262637+ChenYao0203@users.noreply.github.com>
2024-05-14 16:54:52 +0800

    Update data.py

--- data.py
+++ data.py
@@ -84,90 +84,9 @@ class FlickrDataset(data.Dataset):
         return len(self.ids)
 
 
-class PrecompDataset(data.Dataset):
-    """
-    Load precomputed captions and image features
-    Possible options: f8k, f30k, coco, 10crop
-    """
-
-    def __init__(self, data_path, data_split, vocab, opt):
-        self.vocab = vocab
-        loc = data_path + '/'
-
-        # Captions
-        self.captions = []
-        token_caption = []
-        with open(loc+'%s_caps.txt' % data_split, 'rb') as f:
-            for line in f:
-                self.captions.append(line.strip())
-                tokens = nltk.tokenize.word_tokenize(str(line.strip()).lower().decode('utf-8'))
-                token_caption.append(tokens)        
-
-        each_cap_lengths = [len(cap) for cap in token_caption]
-        calculate_max_len = max(each_cap_lengths) + 2
-        print(calculate_max_len)
-
-        # Image features
-        self.images = np.load(loc+'%s_ims.npy' % data_split)
-        self.length = len(self.captions)
-        # rkiros data has redundancy in images, we divide by 5, 10crop doesn't
-        if self.images.shape[0] != self.length:
-            self.im_div = 5
-        else:
-            self.im_div = 1
-        # the development set for coco is large and so validation would be slow
-        if data_split == 'dev':
-            self.length = 5000
-
-        self.max_len = opt.max_len
-
-    def __getitem__(self, index):
-        # handle the image redundancy
-        img_id = index/self.im_div
-        image = torch.Tensor(self.images[img_id])
-        caption = self.captions[index]
-        vocab = self.vocab
-
-        # Convert caption (string) to word ids.
-        tokens = nltk.tokenize.word_tokenize(
-            str(caption).lower().decode('utf-8'))
-        caption = []
-        caption.append(vocab('<start>'))
-        caption.extend([vocab(token) for token in tokens])
-        caption.append(vocab('<end>'))
-        target = torch.Tensor(caption)
 
 
-        ##### deal with caption model data
-        # label = np.zeros(self.max_len)
-        mask = np.zeros(self.max_len + 1)
-        gts = np.zeros((self.max_len + 1))
-
-        # print(tokens)
-        cap_caption = ['<start>'] + tokens + ['<end>']
-        # print(cap_caption)
-        if len(cap_caption) > self.max_len - 1:
-            cap_caption = cap_caption[:self.max_len]
-            cap_caption[-1] = '<end>'
-            
-        for j, w in enumerate(cap_caption):
-            gts[j] = vocab(w)
-
-        non_zero = (gts == 0).nonzero()
-
-
-        mask[:int(non_zero[0][0]) + 1] = 1
-
-
-
-
-        caption_label = torch.from_numpy(gts).type(torch.LongTensor)
-        caption_mask = torch.from_numpy(mask).type(torch.FloatTensor)
-
-        return image, target, index, img_id, caption_label, caption_mask
-
-    def __len__(self):
-        return self.length
+   
 
 
 def collate_fn(data):
@@ -230,20 +149,6 @@ def get_loader_single(data_name, split, root, json, vocab, transform,
     return data_loader
 
 
-def get_precomp_loader(data_path, data_split, vocab, opt, batch_size=100,
-                       shuffle=True, num_workers=2):
-    """Returns torch.utils.data.DataLoader for custom coco dataset."""
-    dset = PrecompDataset(data_path, data_split, vocab, opt)
-
-    # print(dset.max_len)
-    # opt.max_len = dset.max_len + 1
-
-    data_loader = torch.utils.data.DataLoader(dataset=dset,
-                                              batch_size=batch_size,
-                                              shuffle=shuffle,
-                                              pin_memory=True,
-                                              collate_fn=collate_fn)
-    return data_loader
 
 
 def get_transform(data_name, split_name, opt):
@@ -265,32 +170,27 @@ def get_transform(data_name, split_name, opt):
 
 def get_loaders(data_name, vocab, crop_size, batch_size, workers, opt):
     dpath = os.path.join(opt.data_path, data_name)
-    if opt.data_name.endswith('_precomp'):
-        train_loader = get_precomp_loader(dpath, 'train', vocab, opt,
-                                          batch_size, True, workers)
-        val_loader = get_precomp_loader(dpath, 'dev', vocab, opt,
-                                        batch_size, False, workers)
-    else:
-        # Build Dataset Loader
-        roots, ids = get_paths(dpath, data_name, opt.use_restval)
+  
+    # Build Dataset Loader
+    roots, ids = get_paths(dpath, data_name, opt.use_restval)
 
-        transform = get_transform(data_name, 'train', opt)
-        train_loader = get_loader_single(opt.data_name, 'train',
-                                         roots['train']['img'],
-                                         roots['train']['cap'],
-                                         vocab, transform, ids=ids['train'],
-                                         batch_size=batch_size, shuffle=True,
-                                         num_workers=workers,
-                                         collate_fn=collate_fn)
+    transform = get_transform(data_name, 'train', opt)
+    train_loader = get_loader_single(opt.data_name, 'train',
+                                     roots['train']['img'],
+                                     roots['train']['cap'],
+                                     vocab, transform, ids=ids['train'],
+                                     batch_size=batch_size, shuffle=True,
+                                     num_workers=workers,
+                                     collate_fn=collate_fn)
 
-        transform = get_transform(data_name, 'val', opt)
-        val_loader = get_loader_single(opt.data_name, 'val',
-                                       roots['val']['img'],
-                                       roots['val']['cap'],
-                                       vocab, transform, ids=ids['val'],
-                                       batch_size=batch_size, shuffle=False,
-                                       num_workers=workers,
-                                       collate_fn=collate_fn)
+    transform = get_transform(data_name, 'val', opt)
+    val_loader = get_loader_single(opt.data_name, 'val',
+                                   roots['val']['img'],
+                                   roots['val']['cap'],
+                                   vocab, transform, ids=ids['val'],
+                                   batch_size=batch_size, shuffle=False,
+                                   num_workers=workers,
+                                   collate_fn=collate_fn)
 
     return train_loader, val_loader
 
@@ -298,20 +198,17 @@ def get_loaders(data_name, vocab, crop_size, batch_size, workers, opt):
 def get_test_loader(split_name, data_name, vocab, crop_size, batch_size,
                     workers, opt):
     dpath = os.path.join(opt.data_path, data_name)
-    if opt.data_name.endswith('_precomp'):
-        test_loader = get_precomp_loader(dpath, split_name, vocab, opt,
-                                         batch_size, False, workers)
-    else:
-        # Build Dataset Loader
-        roots, ids = get_paths(dpath, data_name, opt.use_restval)
 
-        transform = get_transform(data_name, split_name, opt)
-        test_loader = get_loader_single(opt.data_name, split_name,
-                                        roots[split_name]['img'],
-                                        roots[split_name]['cap'],
-                                        vocab, transform, ids=ids[split_name],
-                                        batch_size=batch_size, shuffle=False,
-                                        num_workers=workers,
-                                        collate_fn=collate_fn)
+    # Build Dataset Loader
+    roots, ids = get_paths(dpath, data_name, opt.use_restval)
+
+    transform = get_transform(data_name, split_name, opt)
+    test_loader = get_loader_single(opt.data_name, split_name,
+                                    roots[split_name]['img'],
+                                    roots[split_name]['cap'],
+                                    vocab, transform, ids=ids[split_name],
+                                    batch_size=batch_size, shuffle=False,
+                                    num_workers=workers,
+                                    collate_fn=collate_fn)
 
     return test_loader
